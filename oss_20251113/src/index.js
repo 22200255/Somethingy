64a3e61d5dab611a27185e5809523e732fbd39c1
22200255 류지성 <ktyel9888@naver.com>
2025-11-13 19:00:51 +0900

    11/13

---
+++ oss_20251113/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);*/
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+const x = 5; 
+let text = "Goodbye"; 
+if (x < 10) { 
+  text = "Hello"; 
+} 
+ const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
+root.render(myElement);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
